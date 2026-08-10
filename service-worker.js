42239fec833d29997fa205bb8a76e2efd07fe887
hebaalmarema1-hub <hebaalmarema1@gmail.com>
2026-08-10 21:19:26 +0200

    Update service worker cache version and files

--- service-worker.js
+++ service-worker.js
@@ -1,14 +1,16 @@
-const CACHE_NAME = "physiovision-ai-v1";
+const CACHE_NAME = "physiovision-ai-v2";
 
 const FILES_TO_CACHE = [
   "./",
   "./index.html",
   "./welcome.html",
   "./manifest.json",
-  "./IMG_0282.PNG"
+  "./IMG_0282.PNG",
+  "./IMG_0684.PNG"
 ];
 
 self.addEventListener("install", event => {
+
   event.waitUntil(
     caches.open(CACHE_NAME).then(cache => {
       return cache.addAll(FILES_TO_CACHE);
@@ -16,26 +18,60 @@ self.addEventListener("install", event => {
   );
 
   self.skipWaiting();
+
 });
 
+
 self.addEventListener("activate", event => {
+
   event.waitUntil(
-    caches.keys().then(keys =>
-      Promise.all(
-        keys
-          .filter(key => key !== CACHE_NAME)
-          .map(key => caches.delete(key))
-      )
-    )
+
+    caches.keys().then(keys => {
+
+      return Promise.all(
+
+        keys.map(key => {
+
+          if (key !== CACHE_NAME) {
+            return caches.delete(key);
+          }
+
+        })
+
+      );
+
+    })
+
   );
 
   self.clients.claim();
+
 });
 
+
 self.addEventListener("fetch", event => {
+
   event.respondWith(
-    caches.match(event.request).then(response => {
-      return response || fetch(event.request);
-    })
+
+    fetch(event.request)
+      .then(response => {
+
+        const responseClone = response.clone();
+
+        caches.open(CACHE_NAME).then(cache => {
+          cache.put(event.request, responseClone);
+        });
+
+        return response;
+
+      })
+
+      .catch(() => {
+
+        return caches.match(event.request);
+
+      })
+
   );
+
 });
